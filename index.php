33b85b0594bfa6fe111d516d7f3b52dff0d2dd13
lquintero <lquintero@vmc.la>
2019-01-18 18:27:37 -0500

    init

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Encuesta</title>
+    <link rel="stylesheet" href="app/css/style.css">
+    <link rel="stylesheet" href="app/css/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+</head>
+
+<body>
+    <!-- <div id="appPollHistory">
+        <poll question-one="¿Está satisfecho con el lote abjudicado?" question-two="¿Como calificarias tu experiencia con Olva Curier?"
+            question-three="Dejanos tu testimonio" client-name="Olva Curier" client-id="98" sid="3" type-poll="" from="session"
+            ep-id-one="2" ep-id-two="1" ep-id-three="3" type-question-one="2" type-question-two="2" type-question-three="1"
+            poll-type="vendedor" img="app/img/imgComprador.png">
+        </poll>
+    </div> -->
+    <div>
+        <div class="content-sbs">
+            <div class="header">
+                <div class="date_sbs">
+                    Miércoles, 14 de Enero <span class="from_sbs">Desde</span> 02:00 PM
+                </div>
+                <div class="info_sbs">
+                    <div class="tittle-sbs">
+                        Operaciones y Servicios Logisticos JR
+                    </div>
+                    <div class="count-sbs">
+                        (30 Ofertas)
+                    </div>
+                </div>
+                <div class="rate_sbs">
+                    3.9 Bueno ***** Calificaciones: 160
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+
+<script src="app/js/header.js"></script>
+
+</html>
